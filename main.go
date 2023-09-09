330797c693aed40083a0e8bbe37dd20e54b4312f
Antonio Souza <antonio.eschola@gmail.com>
2023-09-09 14:11:20 -0400

    chore: allow cors

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -9,21 +10,31 @@ import (
 
 type Payload struct {
 	// TODO: Phrases to be translated
-	Phrase string `json:"phrase"`
+	Text string `json:"Text"`
 }
 
 type Response struct {
-	TransLatedPhrase string `json:"translated_phrase"`
+	Message string `json:"translated_phrase"`
 }
 
 type ResponseError struct {
 	Message string `json:"message"`
 }
 
+const apiTranslationURL = "https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&from=en&to=pt"
+const subscriptionKey = "138aff8d95b748468016c66926bb09c7"
+const location = "eastus"
+
 func main() {
 	// Start the web server using net/http
 
-	http.HandleFunc("/", translateTexts)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// Set the content type header
+		w.Header().Set("Content-Type", "application/json")
+
+		// Write the response body
+		json.NewEncoder(w).Encode(Response{Message: "OK"})
+	})
 
 	// Post endpoint to receive the phrase to be translated
 	http.HandleFunc("/question", answerQuestion)
@@ -33,11 +44,12 @@ func main() {
 	http.ListenAndServe(":9999", nil)
 }
 
-// TODO: Implement the answerQuestion function to work with the /question endpoint in POST method
 func answerQuestion(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
 		// Set the content type header
 		w.Header().Set("Content-Type", "application/json")
+		// allow CORS
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 
 		// Get the request body
 		defer r.Body.Close() // Close the body when we're done
@@ -47,6 +59,21 @@ func answerQuestion(w http.ResponseWriter, r *http.Request) {
 			json.NewEncoder(w).Encode(ResponseError{Message: "Internal Server Error"})
 			return
 		}
+		fmt.Println(string(body))
+		// extract the phrase from the request body and attach it to the payload and send to transtate the text
+		textToBeTranslated := Payload{Text: string(body)}
+
+		// translate the phrase using our function translateTexts and return the translated phrase
+		translatedPhrase, err := translateTexts(textToBeTranslated.Text)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			json.NewEncoder(w).Encode(ResponseError{Message: "Internal Server Error"})
+			return
+		}
+
+		// Write the response body
+		json.NewEncoder(w).Encode(Response{Message: translatedPhrase})
+
 		// Write the same body back as the response
 		w.WriteHeader(http.StatusOK)
 		w.Write(body)
@@ -56,18 +83,49 @@ func answerQuestion(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func translateTexts(w http.ResponseWriter, r *http.Request) {
-	// read the request body
-	// decode the request body
-	// translate the phrase
-	// encode the response
-	// write the response
-	// response := Response{TransLatedPhrase: "Hello World"}
-	responseEncoded := []byte(Response{TransLatedPhrase: "Hello API"}.TransLatedPhrase)
+func translateTexts(phrase string) (string, error) {
+	payload := Payload{Text: "I would really like to drive your car around the block a few times."}
+	payloadBytes, err := json.Marshal(payload)
+	if err != nil {
+		fmt.Println(err)
+		return "", err
+	}
+	// create the http request to translate the phrase
+	req, err := http.NewRequest("POST", apiTranslationURL, bytes.NewBuffer(payloadBytes))
+	if err != nil {
+		fmt.Println(err)
+		return "", err
+	}
+	// set the request headers
+	req.Header.Add("Accept", "*/*")
+	// req.Header.Add("User-Agent", "Thunder Client (https://www.thunderclient.com)")
+	req.Header.Add("Ocp-Apim-Subscription-Key", subscriptionKey)
+	req.Header.Add("Ocp-Apim-Subscription-Region", location)
+	req.Header.Add("Content-Type", "application/json")
 
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	// return the response in json format
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return "", err
+	}
+	defer resp.Body.Close()
 
-	w.Write(responseEncoded)
+	// read the response body
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return "", err
+	}
+
+	// parse the response body
+	var response []Response
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		fmt.Println(err)
+		return "", err
+	}
+
+	// return the translated phrase
+	return response[0].Message, nil
 }
